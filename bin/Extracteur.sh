ed6d39faf3f8e035993a630f0fc469c48887ed8d
jarod SUE <jarodsue@yahoo.fr>
2021-06-07 14:13:05 +0200

    First push of the project
    First Push

---
+++ bin/Extracteur.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+java -cp bin\;ExtracteurConversation/lib/guava-20.0.jar\;ExtracteurConversation/lib/commons-cli-1.4.jar\;ExtracteurConversation/lib/plume.jar\;ExtracteurConversation/guava-25.0-jre.jar main.Main $*
