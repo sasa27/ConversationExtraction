98955affe2a17b4c1d542321e5b82651704d0042
JarodSue <85482830+JarodSue@users.noreply.github.com>
2021-10-15 10:40:57 +0200

    /
    / en trop

--- src/keyDecision/GroupOfAllFilesMinimized.java
+++ src/keyDecision/GroupOfAllFilesMinimized.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class GroupOfAllFilesMinimized {
 	public static ArrayList<GroupOfKeysInOneFile> groupOfAllFiles;
-	/**
+	/*
 	* Represent a group of every keys found in all the files after minimization
 	*/
 	public GroupOfAllFilesMinimized() {
