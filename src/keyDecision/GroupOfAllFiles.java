a6d0b8db7bb885345533766c6da03ecc0192ab1e
Jarod SUE <jarodsue@yahoo.fr>
2021-10-15 10:38:19 +0200

    C:/Program Files/Git/ en trop

--- src/keyDecision/GroupOfAllFiles.java
+++ src/keyDecision/GroupOfAllFiles.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class GroupOfAllFiles {
 	
 	public static ArrayList<GroupOfKeysInOneFile> groupOfAllFiles;
-	/**
+	/*
 	* Represent a group of every keys found in all the files
 	*/
 	public GroupOfAllFiles() {
